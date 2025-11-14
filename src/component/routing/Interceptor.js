65ffd4cd8450379f46cbd968b02cf68a63e592f2
Aliyu Sahabo Adam <aliyusahaboadam@gmail.com>
2025-11-14 17:53:40 +0100

    ss

--- src/component/routing/Interceptor.js
+++ src/component/routing/Interceptor.js
@@ -6,10 +6,12 @@ const api = axios.create({ baseURL: "/api" });
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response && err.response.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "#/school/login";
-    }
+  
+    if (err.response && (err.response.status === 401 || err.response.status === 403)) {
+  localStorage.removeItem("token");
+  window.location.href = "#/school/login";
+  }
+ 
     return Promise.reject(err);
   }
 );
